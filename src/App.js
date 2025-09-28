22d1cdbcc39c15458817f107feeaddc6567f6c27
Matthew-IR <67317204+Matthew-IR@users.noreply.github.com>
2025-09-28 16:05:26 +0100

    Squashed commit of the following:
    commit 23d1c7fdd9adc371d8d592342815270669f7e60e
    Merge: d7a2619 7c2e978
    Author: Matthew-IR <67317204+Matthew-IR@users.noreply.github.com>
    Date:   Sun Sep 28 16:01:53 2025 +0100
    
        Merge remote-tracking branch 'origin/main' into recipegallery
    
    commit d7a2619c958329eb30cd17486ba1605b81961223
    Author: Matthew-IR <67317204+Matthew-IR@users.noreply.github.com>
    Date:   Sun Sep 28 15:54:12 2025 +0100
    
        Add initial gallery fetching data from API and showing image + description

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { InputField } from './component/input_field.js';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useState } from 'react';
 import { MultipleSelect } from './component/multiple-select.js';
 import { FormWrapper } from './component/form-wrapper.js';
-import { Navbar } from './component/nav.js';
+import { Navbar } from './component/nav.js';import { RecipeGallery } from './components/recipegallery/recipegallery';
+
  
 function App() { 
   const key = process.env.REACT_APP_SPOON_KEY;
   const ingredientsInclude = "burger";
   const ingredientsExclude = "rice";
+  const [Recipes, setRecipes] = useState([]);
 
   const url = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${ingredientsInclude}&excludeIngredients=${ingredientsExclude}&addRecipeInformation=true&addRecipeInstructions&number=5&apiKey=${key}`;
 
@@ -18,6 +20,8 @@ function App() {
         if (!response.ok){throw new Error("Could not connect")}
         const data = await response.json();
         console.log(data);
+
+        setRecipes(data.results);
     } catch (e) {
         console.log('Could not search properly: ' + e);
     }*/
@@ -34,6 +38,8 @@ function App() {
       <FormWrapper>
       </FormWrapper>
       
+
+      <RecipeGallery items={Recipes}></RecipeGallery>
     </div>
   );
 }
